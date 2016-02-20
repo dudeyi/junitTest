9353dbbf0b64a61d88f2b029e399903888b59f07
dudeyi <740935561@qq.com>
2016-02-20 17:43:08 +0800

    2016-2-20 17:40 重新提交demo

---
+++ src/com/junitTest/Calculator.java
@@ -0,0 +1,53 @@
+package com.junitTest;
+
+public class Calculator extends Thread{
+
+    /**
+     * 加法
+     * @param arg1
+     * @param arg2
+     * @return
+     */
+    public int add(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+    
+    /**
+     * 减法
+     * @param arg1
+     * @param arg2
+     * @return
+     */
+    public int sub(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+    
+    /**
+     * 乘法
+     * @param arg1
+     * @param arg2
+     * @return
+     */
+    public int mul(int arg1, int arg2) {
+        return arg1 * arg2;
+    }
+    
+    /**
+     * 除法
+     * @param arg1
+     * @param arg2
+     * @return
+     */
+    public int div(int arg1, int arg2) {
+        return arg1 / arg2;
+    }
+    
+    public void timeOut(){
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
